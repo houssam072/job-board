5235afa294dd48c9fe37d8b97a03eb1eacdad63c
houssam072 <houssamsaleh072@gmail.com>
2023-08-01 04:58:07 -0700

    add job views & url

--- job/admin.py
+++ job/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Jop
+from .models import *
 
 admin.site.register(Jop)
+admin.site.register(Category)
--- job/models.py
+++ job/models.py
@@ -17,6 +17,13 @@ class Jop(models.Model):
     vacancy = models.IntegerField(default=1)
     salary = models.IntegerField(default=0)
     experience = models.IntegerField(default=1)
+    Category = models.ForeignKey('Category',on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
+    
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
